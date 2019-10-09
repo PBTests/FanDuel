15178cf8e5347010fbd3c93d7664b6292e93bcc8
unknown <paul@buon.co.uk>
2019-10-09 11:12:17 +0100

    Error page fix

--- pages/_error.js
+++ pages/_error.js
@@ -2,7 +2,6 @@ import React from "react";
 import HTTPStatus from "http-status";
 import Head from "next/head";
 import PageHeading from "../components/Global/PageHeading";
-import Header from "../components/Header";
 import styles from "../components/Global/global.scss";
 
 function Error(props) {
@@ -15,11 +14,9 @@ function Error(props) {
   return (
     <div>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Error</title>
       </Head>
       <div>
-        <Header />
         <PageHeading>Oh dear! We have a slight problem...</PageHeading>
         <div className="wide-content">
           <div className="container">
