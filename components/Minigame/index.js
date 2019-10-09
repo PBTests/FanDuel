e9f32ccee5b09b07836414fd1b24b7445b0673a6
unknown <paul@buon.co.uk>
2019-10-09 12:57:56 +0100

    Randomize and select results from JSON response

--- components/Minigame/index.js
+++ components/Minigame/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import fetch from "isomorphic-unfetch";
 
+const PLAYERS_DISPLAY_COUNT = 6;
 const API_URL =
   "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json";
 
@@ -9,10 +10,10 @@ class MinigameContainer extends React.Component {
     super(props);
     this.state = {};
 
-    this.fetchPlayers = this.fetchPlayers.bind(this);
+    this.startGame = this.startGame.bind(this);
   }
 
-  fetchPlayers = () => {
+  startGame = () => {
     console.warn("Fetching data...");
 
     // Set Loading state
@@ -29,13 +30,49 @@ class MinigameContainer extends React.Component {
 
     fetch(API_URL)
       .then(r => r.json())
-      .then(data => {});
+      .then(data => {
+        console.log("RES:", data);
+
+        var players = data.players ? data.players : {};
+
+        // Randomize the results
+        if (players) players = players.sort(() => 0.5 - Math.random());
+
+        // Cut results to size
+        if (players) players = players.slice(0, PLAYERS_DISPLAY_COUNT);
+
+        console.log("players:", players);
+
+        this.setState({
+          playerSearch: {
+            isLoaded: true,
+            isLoading: false,
+            isError: false,
+            players: players
+          }
+        });
+      });
   };
 
   render() {
     return (
       <React.Fragment>
-        <div className="wide-content">Minigame container</div>
+        <div className="wide-content">
+          <div className="container">
+            <h2>Objective</h2>
+            <p>You will be presented with {PLAYERS_DISPLAY_COUNT} players. </p>
+            <p>
+              Choose which player you think has the highest FPPG (Fanduel Points
+              Per Game).
+            </p>
+            <center>
+              <a className="form-button" onClick={this.startGame}>
+                Start Game
+              </a>
+            </center>
+            &nbsp;
+          </div>
+        </div>
       </React.Fragment>
     );
   }
